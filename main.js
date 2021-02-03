bb0e4512f57ba7f045e5bd7c7ee7747f3129c241
Ewuentertainment <65340309+Ewuentertainment@users.noreply.github.com>
2021-02-03 09:20:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    window.open("https://www.w3schools.com",true);
+    window.open("https://www.youraddress.com","_self")
 }
